2c46afb27e5bfe1e5416e362079ccd3c24ea7d1f
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-05-09 16:06:39 +0300

    server running

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const config = require('./config/data');
+
+// Init app
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('Invalid entrypoint');
+})
+
+// Listen to port
+app.listen(config.port, () => {
+    console.log('Server started on port ' + config.port);
+});
